1f42e3c3c4bc472f6e25ace19a834627f5a004c7
Sho Kusano <rosylilly@aduca.org>
2012-07-27 14:14:14 +0900

    resolve change

--- lib/git_hook/hook.rb
+++ lib/git_hook/hook.rb
@@ -8,7 +8,7 @@ module GitHook
       name.sub(/^[A-Z]/) { | match |
         match.downcase
       }.gsub(/::([A-Z])/) { | match |
-        "-#{$1.to_s.downcase}"
+        "/#{$1.to_s.downcase}"
       }.gsub(/[A-Z]/) { | match |
         "_#{match.downcase}"
       }
@@ -17,7 +17,7 @@ module GitHook
     def self.class_name(name)
       name.sub(/^[a-z]/) { | match |
         match.upcase
-      }.gsub(/\-([a-z])/) { | match |
+      }.gsub(/[\/\-]([a-z])/) { | match |
         "::#{$1.to_s.upcase}"
       }.gsub(/_([a-z])/) { | match |
         $1.to_s.upcase
--- spec/lib/git_hook/hook_spec.rb
+++ spec/lib/git_hook/hook_spec.rb
@@ -3,7 +3,7 @@ require 'git_hook/hook'
 
 describe GitHook::Hook do
   it ".gem_name" do
-    GitHook::Hook.gem_name('Bundler::SetUp').should == 'bundler-set_up'
+    GitHook::Hook.gem_name('Bundler::SetUp').should == 'bundler/set_up'
   end
 
   it ".class_name" do
